5d288ee7f442c57b7020b18393d116925008c79d
Jonas Peters <jonas.peters123@gmail.com>
2024-05-13 15:19:34 -0300

    deveras

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    
+    name='hardarda',
+    version='4.2.0',
+    description='This is a project aimed at overriding the default admin templates of Django 4.2, using Bootstrap 5 as the base for the new templates.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://bitbucket.org/jonaspeters/hardrada',
+    author='Jonas Peters',
+    author_email='jonas.peters123@gmail.com',
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords='django, plugin, bootstrap, admin, templates',
+    packages=find_packages(),
+    python_requires='>=3.10',
 )
---
+++ src/hardrada/conf/settings.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+HARDRADA_BASE_DIR = Path(__file__).resolve().parent.parent
+
+HARDRADA_TEMPLATES = [
+    HARDRADA_BASE_DIR / 'contrib' / 'templates'
+]
+
+HARDRADA_STATICFILES_DIRS = [
+    HARDRADA_BASE_DIR / 'contrib' / 'staticfiles'
+]
