1b3e97354b27455b76b40863d72938edbb34bab3
Jonas Peters <jonas.peters123@gmail.com>
2024-05-28 17:40:04 -0300

    login and base templates updated

--- src/hardrada/staticfiles/admin/js/jquery.actions.js
+++ src/hardrada/staticfiles/admin/js/jquery.actions.js
@@ -1,5 +1,36 @@
 (function($) {
     $(function() {
-        
+        /**
+         * Menu actions
+         */
+        $menu = $(".menu");
+        $content = $(".content");
+
+        $menu.find(".navbar a").on("click", function() {
+            if ($menu.hasClass("menu-extended")) {
+                $menu.find(".app-name, .app-arrow").hide();
+                $menu.find(".app-model").removeClass("show");
+                
+                $menu.animate({width: '3.1rem'}, function() {
+                    $menu.removeClass("menu-extended");
+                    $menu.addClass("menu-collapsed");
+                });
+                $content.animate({"margin-left": '3.1rem'});
+            
+            } else {
+                $menu.animate({width: '15rem'}, function() {
+                    $menu.find(".app-name, .app-arrow").show();
+                    $menu.addClass("menu-extended");
+                    $menu.removeClass("menu-collapsed");
+                });
+                $content.animate({"margin-left": '15rem'});
+            }
+        });
+
+        $menu.find(".app-model").on("show.bs.collapse", function() {
+            if (!$menu.hasClass("menu-extended")) {
+                $menu.find(".navbar a").click();
+            }
+        })
     })
 })(jQuery)
